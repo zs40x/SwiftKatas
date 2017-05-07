73720f6cc8255811f14931431dfa38eb99449e36
Stefan Mehnert <StefanMehnert@gmail.com>
2017-05-07 09:41:06 +0200

    Empty implementation with a testClass and a first passing test for initial values

--- StringCalculator.playground/Contents.swift
+++ StringCalculator.playground/Contents.swift
@@ -1,5 +1,25 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+import XCTest
 
 var str = "Hello, playground"
+
+class StringCalculator {
+    
+    func add(_ a: String, _ b: String) -> Int {
+        
+        return 0
+    }
+}
+
+class StringCalculatorTest: XCTestCase {
+    
+    let testInstance = StringCalculator()
+    
+    func testInitialStringsReturn0() {
+        XCTAssertEqual(testInstance.add("", ""), 0)
+    }
+}
+
+StringCalculatorTest.defaultTestSuite().run()
