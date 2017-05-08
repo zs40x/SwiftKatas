a8d3b97c8ade2b810c3eeec51041df57d2801b4c
Stefan Mehnert <StefanMehnert@gmail.com>
2017-05-08 05:48:57 +0200

    Threws an error if the number is negative

--- StringCalculator.playground/Contents.swift
+++ StringCalculator.playground/Contents.swift
@@ -5,16 +5,24 @@ import XCTest
 
 var str = "Hello, playground"
 
+enum StringCalculationError: Error {
+    case negativeNumber
+}
+
 class StringCalculator {
     
     private let seperators = ",\n"
     
-    func add(_ numbers: String) -> Int {
+    func add(_ numbers: String) throws -> Int {
         
-        return numbers
+        return try numbers
                 .components(separatedBy: CharacterSet(charactersIn: seperators))
                 .reduce(0, ({ sum, current in
                     guard let nextInteger = Int(current) else { return sum }
+                    
+                    if nextInteger < 0 {
+                        throw StringCalculationError.negativeNumber
+                    }
                 
                     return sum + nextInteger
                 }))
@@ -27,23 +35,27 @@ class StringCalculatorTest: XCTestCase {
     let testInstance = StringCalculator()
     
     func testInitialStringsReturn0() {
-        XCTAssertEqual(testInstance.add(""), 0)
+        XCTAssertEqual(try testInstance.add(""), 0)
     }
     
     func testAddToStringIntsReturnsSum() {
-        XCTAssertEqual(testInstance.add("1,1"), 2)
+        XCTAssertEqual(try testInstance.add("1,1"), 2)
     }
     
     func testNonIntegerInStringIsIgnored() {
-        XCTAssertEqual(testInstance.add("1,b,3"), 4)
+        XCTAssertEqual(try testInstance.add("1,b,3"), 4)
     }
     
     func testSingleIntegerIsReturned() {
-        XCTAssertEqual(testInstance.add("1"), 1)
+        XCTAssertEqual(try testInstance.add("1"), 1)
     }
     
     func testAddsWithNewLineAsSeperator() {
-        XCTAssertEqual(testInstance.add("2\n2"), 4)
+        XCTAssertEqual(try testInstance.add("2\n2"), 4)
+    }
+    
+    func testNegativeRaisesException() {
+        XCTAssertThrowsError(try testInstance.add("0,-1,1"))
     }
 }
 
