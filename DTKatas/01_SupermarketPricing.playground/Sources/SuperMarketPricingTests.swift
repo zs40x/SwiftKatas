145e0e2e654ce82ec3da2252f8dce6f5bacb8eca
SMX <stefanmehnert@gmail.com>
2018-05-07 08:06:56 +0200

    Initial appProject - shoppingApp

--- DTKatas/01_SupermarketPricing.playground/Sources/SuperMarketPricingTests.swift
+++ DTKatas/01_SupermarketPricing.playground/Sources/SuperMarketPricingTests.swift
@@ -4,7 +4,13 @@ import XCTest
 public class SupermarketPricingTests: XCTestCase {
     
     public func testCheckoutWithoutItems() {
-        
         XCTAssertEqual(0.0, Checkout().total([Item]()))
+    }
+    
+    func testCheckoutSingleItem() {
+        XCTAssertEqual(
+            1.0,
+            Checkout()
+                .total([Item(name: "", price: 1.0)]))
     }
 }
